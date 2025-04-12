aff832a3c104d4d29cc9eb3787da05da222ba73b
mashab246 <musagaliwango64@gmail.com>
2025-04-12 07:48:35 +0300

    Update movie_library.py

--- movie_library.py
+++ movie_library.py
@@ -9,3 +9,17 @@ sales = {}
 # Function to add a movie
 def add_movie(title, genre, release_date, actors, description, price):
     # Generate a unique random movie number
+    while True:
+        movie_number = random.randint(1000, 9999)
+        if movie_number not in movies:
+            break
+
+    movies[movie_number] = {
+        'title': title,
+        'genre': genre,
+        'release_date': release_date,
+        'actors': actors.split(", "),
+        'description': description,
+        'price': price
+    }
+    print(f'Movie "{title}" added successfully with Movie Number: {movie_number}!')
